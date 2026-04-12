8e8c1839bb45fd4faf2f5968847e642602350269
Abhinay Reddy <xprmnts@users.noreply.github.com>
2026-04-11 20:47:14 -0400

    Add: minimum-window-with-characters - submission-2

---
+++ Data Structures & Algorithms/minimum-window-with-characters/submission-2.py
@@ -0,0 +1,37 @@
+from collections import Counter
+
+class Solution:
+    def minWindow(self, s: str, t: str) -> str:
+        if not t or not s:
+            return ""
+        
+        count_t = Counter(t)
+        count_w = {}
+
+        have, need = 0, len(count_t)
+        res, res_len = [-1, -1], float("inf")
+        l = 0
+
+        for r, char in enumerate(s):
+            if char in count_t:
+                count_w[char] = count_w.get(char, 0) + 1
+                if count_t[char] == count_w[char]:
+                    have += 1
+            
+            while have == need:
+                if (r - l + 1) < res_len:
+                    res = [l, r]
+                    res_len = r - l + 1
+                
+                left_char = s[l]
+                if left_char in count_t:
+                    count_w[left_char] -= 1
+                    if count_w[left_char] < count_t[left_char]:
+                        have -= 1
+                l += 1
+        
+        l, r = res
+        return s[l: r + 1] if res_len != float("inf") else ""
+
+
+
